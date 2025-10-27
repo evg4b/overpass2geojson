f2c85bf6151bc07f36f5c4733a85a2aca6198525
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-10-27 15:37:12 -0400

    Cleanup

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,29 +1,6 @@
 #!/usr/bin/env node
 import React from 'react';
 import {render} from 'ink';
-import meow from 'meow';
 import App from './app.js';
 
-const cli = meow(
-	`
-	Usage
-	  $ overpass2geojson
-
-	Options
-		--name  Your name
-
-	Examples
-	  $ overpass2geojson --name=Jane
-	  Hello, Jane
-`,
-	{
-		importMeta: import.meta,
-		flags: {
-			name: {
-				type: 'string',
-			},
-		},
-	},
-);
-
-render(<App name={cli.flags.name} />);
+render(<App name='lololo' />);
